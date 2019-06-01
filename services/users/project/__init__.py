54f94fed87c18c43c41b3b4d23be513aa9117463
Cyrus Villacampa <chairus06@gmail.com>
2019-06-01 20:37:10 +0930

    Add postgres database to manage users and dockerized users and postgres services

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,10 +1,11 @@
 # services/users/project/__init__.py
 
+import os
+import sys
 from flask import Flask, jsonify
 from flask_restful import Resource, Api
+from flask_sqlalchemy import SQLAlchemy
 
-# debugging purposes
-#print(__name__)
 
 # instantiate the app
 app = Flask(__name__)
@@ -12,14 +13,32 @@ app = Flask(__name__)
 api = Api(app)
 
 # set config
-app.config.from_object('project.config.DevelopmentConfig')
+app_settings = os.getenv('APP_SETTINGS')
+app.config.from_object(app_settings)
+
+# instantiate the db (postgres)
+db = SQLAlchemy(app)
+
+# model
+class User(db.Model):
+	__tablename__ = 'users'
+	id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+	username = db.Column(db.String(128), nullable=False)
+	email = db.Column(db.String(128), nullable=False)
+	active = db.Column(db.Boolean(), default=True, nullable=False)
+
+	def __init__(self, username, email):
+		self.username = username
+		self.email = email
+
+# print(app.config, file=sys.stderr)
 
 # create our own "Resource" class by extending Resource. UsersPing extends Resource
 class UsersPing(Resource):
 	def get(self):
 		return {
-		'status': 'success',
-		'message': 'pong!'
+			'status': 'success',
+			'message': 'pong!'
 		}
 
 api.add_resource(UsersPing, '/users/ping')
