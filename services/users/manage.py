ef77ecad0992b09f35d8fb344e7cccb5fdd47c56
Cyrus Villacampa <chairus06@gmail.com>
2019-06-02 18:58:35 +0930

    Add comments

--- services/users/manage.py
+++ services/users/manage.py
@@ -15,9 +15,12 @@ def recreate_db():
 	db.create_all()
 	db.session.commit()
 
+"""Runs the tests
+
+Runs the tests without code coverage
+"""
 @cli.command()
 def test():
-	"""Runs the tests without code coverage"""
 	tests = unittest.TestLoader().discover('project/tests', pattern='test*.py')
 	result = unittest.TextTestRunner(verbosity=2).run(tests)
 	if result.wasSuccessful():
--- services/users/project/config.py
+++ services/users/project/config.py
@@ -2,25 +2,34 @@
 
 import os
 
+"""Base configuration
+
+Common configuration for all types of environment
+"""
 class BaseConfig:
-	"""Base configuration"""
 	TESTING = False
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
 	SECRET_KEY = 'i_dont_want_it'
 	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
+"""Development configuration
 
+Contains configuration for development environment
+"""
 class DevelopmentConfig(BaseConfig):
-	"""Development configuration"""
 	pass
 
+"""Testing configuration
 
+Contains configuration for testing environment
+"""
 class TestingConfig(BaseConfig):
-	"""Testing configuration"""
 	TESTING = True
 	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_TEST_URL')
 
+"""Production configuration
 
+Contains configuration for production environment
+"""
 class ProductionConfig(BaseConfig):
-	"""Production configuration"""
 	pass
