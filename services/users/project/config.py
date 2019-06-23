906d104765fc6d716f222b153bbb74303bb08394
Cyrus Villacampa <chairus06@gmail.com>
2019-06-23 14:15:53 +0930

    Remove flake8 run in travis build script

--- services/users/project/config.py
+++ services/users/project/config.py
@@ -2,34 +2,26 @@
 
 import os
 
-"""Base configuration
-
-Common configuration for all types of environment"""
+"""Base configuration Common configuration for all types of environment"""
 class BaseConfig:
-	TESTING = False
-	SQLALCHEMY_TRACK_MODIFICATIONS = False
-	SECRET_KEY = 'i_dont_want_it'
-	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    TESTING = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = 'i_dont_want_it'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
 
-"""Development configuration
-
-Contains configuration for development environment"""
+"""Development configuration Contains configuration for development environment"""
 class DevelopmentConfig(BaseConfig):
-	pass
+    pass
 
 
-"""Testing configuration
-
-Contains configuration for testing environment"""
+"""Testing configuration Contains configuration for testing environment"""
 class TestingConfig(BaseConfig):
-	TESTING = True
-	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_TEST_URL')
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_TEST_URL')
 
 
-"""Production configuration
-
-Contains configuration for production environment"""
+"""Production configuration Contains configuration for production environment"""
 class ProductionConfig(BaseConfig):
-	pass
+    pass
 
